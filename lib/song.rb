65871dd64e5e7c38af2d909772733f2087840e94
Tam Vu <vutam_a@hotmail.com>
2019-06-17 22:49:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,8 @@ class Song
   end
   
   def self.find_by_name(name)
-    self.all.detect { |song| song.name == name}
+    #self.all.detect { |song| song.name == name}
+    
   end 
   
   def self.find_or_create_by_name(name)
